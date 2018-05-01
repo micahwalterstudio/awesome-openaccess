a7d835f1ec336240dd852db61029b448401d09cb
Micah Walter <micah@micahwalter.com>
2018-05-01 17:25:24 +0000

    added initial script

---
+++ bin/generate-readme.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import os.path
+import yaml
+import json
+import glob
+import pprint
+
+if __name__ == '__main__':
+
+    whoami = os.path.abspath(sys.argv[0])
+    bindir = os.path.dirname(whoami)
+    datadir = os.path.dirname(bindir)
+    datadir = os.path.join(datadir, 'data')
+
+    readme = open('README.md', 'w')
+    
+    readme.write('# Awesome OpenAccess')
+    readme.write('[![Awesome](https://awesome.re/badge-flat.svg)](https://awesome.re)')
+    readme.write('\n')
+    readme.write('*An awesome list of awesome OpenAccess projects*')
+    
+    ## read in all the yml files
+
+    datafiles = {}
+    
+    for filename in glob.glob(datadir + '/*.yml'):
+        stream = file(filename, 'r')
+        yml = yaml.load(stream)
+        head, tail = os.path.split(filename)
+        tail = os.path.splitext(tail)[0]
+        datafiles[tail] = yml
+    
+    for org in sorted(datafiles.iterkeys()):    
+        pprint.pprint(datafiles[org]['name'])
+        readme.write('* [' + datafiles[org]['name'] + '](' + datafiles[org]['url'] + ')\n' )
+
+    readme.close()     
